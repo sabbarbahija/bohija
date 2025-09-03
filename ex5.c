b755ca4c41c6fe562548d6a754cef45ae3b97939
sabbarbahija <bohija.sabbar11@gmail.com>
2025-09-03 12:17:16 +0200

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -4,7 +4,7 @@ int main(){
      printf("entrer le valeur de x");
      scanf("%f",&x);
      poly=3*x*x*x*x*x+2*x*x*x*x-5*x*x*x-2*x*x+7*x-6;
-     printf("la valeur de polynnome est %f";poly);
+     printf("la valeur de polynnome est %f",poly);
      return 0;
 
 }
