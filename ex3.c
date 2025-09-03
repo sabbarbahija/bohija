7ea1d153b2fad14b189a35a2313b4338fc7d6876
sabbarbahija <bohija.sabbar11@gmail.com>
2025-09-03 11:36:35 +0200

    modi

--- ex3.c
+++ ex3.c
@@ -1,12 +1,10 @@
  #include<stdio.h>
 int main(){
-    float v;
-    int r, PI;
-     PI=3,14;
+    float v,PI;
+    int r;
+     PI=3.14;
     printf("donner le rayon de cette sphère :");
-    scanf("%f",&r);
+    scanf("%d",&r);
     v=(4.0f/3.0f)*PI*r*r*r;
-    printf("le volume d'une sphère est :%f",&v);
+    printf("le volume d'une sphère est :%f",v);
  return 0;
-
-}
