ada8680ba5b84cca16d715dd1350c6f23fa64c39
sabbarbahija <bohija.sabbar11@gmail.com>
2025-09-03 12:16:36 +0200

    Update ex4.c

--- ex4.c
+++ ex4.c
@@ -3,9 +3,9 @@
  int  main(){
     float montant,montanttaxe;
     printf("entrer un montant  en dollars et en cents");
-    scanf("%f \n,& montant");
-    montanttaxe = montant * 0,05;
-    printf("montant avec texe de 5% est %f ";montanttaxe);
+    scanf("%f\n ,& montant");
+    montanttaxe = montant * 0.05;
+    printf("montant avec texe de 5% est %f ",montanttaxe);
     return 0;
     
 }
